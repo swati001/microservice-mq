c593a4c5eb5982effd7a236cd80010a40c4c53ff
Swati Sharma <swatiinfo86@gmail.com>
2020-04-14 13:06:14 -0400

    adding the JMSTemplate field

--- src/main/java/com/app/enquiry/EnquiryApplication.java
+++ src/main/java/com/app/enquiry/EnquiryApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
+@EnableJms
 public class EnquiryApplication {
+	
+	@Autowired
+     	private JmsTemplate jmsTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnquiryApplication.class, args);
