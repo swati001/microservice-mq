72abf379753b0754399ab0ddb28e3d332aff1847
Swati Sharma <swatiinfo86@gmail.com>
2020-04-16 00:53:57 +0530

    Update EnquiryController.java

--- src/main/java/com/app/enquiry/controller/EnquiryController.java
+++ src/main/java/com/app/enquiry/controller/EnquiryController.java
@@ -1,5 +1,13 @@
 package com.app.enquiry.controller;
 
+@RestController
 public class EnquiryController {
 
+@Autowired
+Private JmsTemplate jmsTemplate;
+
+@GetMapping(“/send”)
+public String sendMessage(String msg){
+   
+}
 }
