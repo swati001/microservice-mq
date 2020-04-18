16eb3c5f10e1e391e484e310c861a217dff78fdc
Swati Sharma <swatiinfo86@gmail.com>
2020-04-18 07:08:27 -0400

    Update JmsConfiguration.java

--- src/main/java/com/app/enquiry/config/JmsConfiguration.java
+++ src/main/java/com/app/enquiry/config/JmsConfiguration.java
@@ -1,5 +1,22 @@
 package com.app.enquiry.config;
 
 class JmsConfiguration {
+  private static final Logger logger = LogManager.getLogger(JmsConfiguration.class);
+
+  @Bean
+  public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+                          DefaultJmsListenerContainerFactoryConfigurer configurer) {
+    DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+    configurer.configure(factory, connectionFactory);
+    return factory;
+  }
+
+  @Bean 
+  public MessageConverter jacksonJmsMessageConverter() {
+    MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+    converter.setTargetType(MessageType.TEXT);
+    converter.setTypeIdPropertyName("_type");
+    return converter;
+  }
 
 }
