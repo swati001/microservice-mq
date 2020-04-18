dc10a33b7c390d929d150268c8f41bbb4ecb71f5
Swati Sharma <swatiinfo86@gmail.com>
2020-04-18 06:31:16 -0400

    Update EnquiryController.java

--- src/main/java/com/app/enquiry/controller/EnquiryController.java
+++ src/main/java/com/app/enquiry/controller/EnquiryController.java
@@ -6,8 +6,23 @@ public class EnquiryController {
 @Autowired
 Private JmsTemplate jmsTemplate;
 
-@GetMapping(“/send”)
-public String sendMessage(String msg){
-   
+@GetMapping("send")
+public String send(){
+    try{
+        jmsTemplate.convertAndSend("DEV.QUEUE.1", "test");
+        return "SUCCESS";
+    }catch(JmsException ex){
+        ex.printStackTrace();
+        return "FAIL";
+    }
+}
+@GetMapping("receive")
+public String receive(){
+    try{
+        return jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString();
+    }catch(JmsException ex){
+        ex.printStackTrace();
+        return "FAIL";
+    }
 }
 }
