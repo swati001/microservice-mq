403b190f17e35e5f79f462399e7e3941ea4d7d98
Swati Sharma <swatiinfo86@gmail.com>
2020-04-19 00:54:43 -0400

    Create CustomJmsListener.java

---
+++ src/main/java/com/app/enquiry/utils/CustomJmsListener.java
@@ -0,0 +1,36 @@
+package com.app.enquiry.utils;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomJmsListener implements MessageListener {
+
+private static Logger LOG = LoggerFactory.getLogger(CustomJmsListener.class);
+
+@Override
+    public void onMessage(Message message)
+    {
+        LOG.info("CustomJmsListener called");
+        
+        if (message instanceof TextMessage)
+        {
+            try
+            {
+                String msg = ((TextMessage) message).getText();
+                LOG.debug("Message contents: {} ", msg);
+            }
+            catch (Exception e)
+            {
+                LOG.error("Exception in CustomJmsListener: "+ex.getMessage());
+               
+            }
+            
+        }
+        
+    }
+}
